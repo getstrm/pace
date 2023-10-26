8402231cab9e6b2c021ee64451fc5704c4849386
Bob van den Hoogen <bob@strmprivacy.io>
2023-10-26 10:32:36 +0200

    docs: another update to gen script

--- scripts/docs/gendocs.py
+++ scripts/docs/gendocs.py
@@ -5,6 +5,9 @@ APIReference = '\n\n'.join(["# API Reference"] +
     [
         f'''{{% swagger src=".gitbook/assets/openapi.yaml" path="{path}" method="{method}" summary="{spec['paths'][path][method]['operationId'].replace(spec['paths'][path][method]['tags'][0] + "_", "")}" %}}
         [openapi.yaml](.gitbook/assets/openapi.yaml)
+        {{% swagger-description %}}
+
+        {{% endswagger-description %}}
         {{% endswagger %}}'''
         for path in spec['paths']
         for method in spec['paths'][path].keys()
