a9f73ccedecf8aef7522f0319160c4a0a54d3e86
Robin Trietsch <robin@getstrm.com>
2023-11-08 23:42:29 +0100

    feat(pace-17): ensure data policy upsert also includes global transforms

--- app/src/main/kotlin/com/getstrm/pace/service/DataPolicyService.kt
+++ app/src/main/kotlin/com/getstrm/pace/service/DataPolicyService.kt
@@ -1,12 +1,8 @@
 package com.getstrm.pace.service
 
 import build.buf.gen.getstrm.pace.api.entities.v1alpha.DataPolicy
-import build.buf.gen.getstrm.pace.api.entities.v1alpha.DataPolicy.Principal
 import com.getstrm.pace.dao.DataPolicyDao
-import com.getstrm.pace.exceptions.BadRequestException
 import com.getstrm.pace.exceptions.ResourceException
-import com.getstrm.pace.util.pathString
-import com.google.rpc.BadRequest
 import com.google.rpc.ResourceInfo
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -16,13 +12,16 @@ class DataPolicyService(
     private val dataPolicyDao: DataPolicyDao,
     private val processingPlatforms: ProcessingPlatformsService,
     private val dataPolicyValidatorService: DataPolicyValidatorService,
+    private val globalTransformsService: GlobalTransformsService,
 ) {
     suspend fun listDataPolicies(): List<DataPolicy> = dataPolicyDao.listDataPolicies()
 
     @Transactional
     suspend fun upsertDataPolicy(dataPolicy: DataPolicy): DataPolicy {
-        dataPolicyValidatorService.validate(dataPolicy, processingPlatforms.listGroupNames(dataPolicy.platform.id))
-        val newDataPolicy = dataPolicyDao.upsertDataPolicy(dataPolicy)
+        val completeDataPolicy = globalTransformsService.addRuleSet(dataPolicy).also {
+            dataPolicyValidatorService.validate(it, processingPlatforms.listGroupNames(dataPolicy.platform.id))
+        }
+        val newDataPolicy = dataPolicyDao.upsertDataPolicy(completeDataPolicy)
         enforceStatement(newDataPolicy)
         return newDataPolicy
     }
