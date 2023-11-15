2a484f45ea1bbc6f319c3c3be256540dec898581
Ivan Plantevin <ivan@strmprivacy.io>
2023-11-15 11:01:34 +0100

    feat(pace-13): use dollar signs to reference capturing groups in regexp transforms

--- app/src/main/kotlin/com/getstrm/pace/processing_platforms/postgres/PostgresTransformer.kt
+++ app/src/main/kotlin/com/getstrm/pace/processing_platforms/postgres/PostgresTransformer.kt
@@ -1,26 +1,13 @@
-package com.getstrm.pace.processing_platforms.bigquery
+package com.getstrm.pace.processing_platforms.postgres
 
 import build.buf.gen.getstrm.pace.api.entities.v1alpha.DataPolicy
 import com.getstrm.pace.processing_platforms.CAPTURING_GROUP_REGEX
 import com.getstrm.pace.processing_platforms.ProcessingPlatformTransformer
-import com.getstrm.pace.util.defaultJooqSettings
 import com.getstrm.pace.util.fullName
-import org.jooq.DSLContext
 import org.jooq.Field
-import org.jooq.SQLDialect
-import org.jooq.conf.Settings
 import org.jooq.impl.DSL
 
-class BigQueryTransformer(
-    customJooqSettings: Settings.() -> Unit = {},
-) : ProcessingPlatformTransformer {
-    /**
-     * BigQuery requires backticked names for certain names, and MySQL dialect uses backticks, so we abuse this here.
-     */
-    private val bigQueryDsl: DSLContext =
-        DSL.using(SQLDialect.MYSQL, defaultJooqSettings.apply(customJooqSettings))
-
-    override fun renderName(name: String) = bigQueryDsl.renderNamedParams(DSL.quotedName(name))
+class PostgresTransformer : ProcessingPlatformTransformer {
 
     override fun regexpReplace(
         field: DataPolicy.Field,
@@ -34,8 +21,8 @@ class BigQueryTransformer(
                 DSL.`val`(regexp.regexp),
             )
         } else {
-            // Bigquery expects two backslashes for the capturing group notation, here doubled because of Kotlin escaping.
-            val replacementWithBackslashNotation = regexp.replacement.replace(CAPTURING_GROUP_REGEX, """\\\\$1""")
+            // Postgres expects one backslash for the capturing group notation, here doubled because of Kotlin escaping.
+            val replacementWithBackslashNotation = regexp.replacement.replace(CAPTURING_GROUP_REGEX, """\\$1""")
             DSL.regexpReplaceAll(
                 DSL.field(field.fullName(), String::class.java),
                 regexp.regexp,
