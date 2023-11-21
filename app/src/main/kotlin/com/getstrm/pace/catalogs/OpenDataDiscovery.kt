c9634f58a7515b63d0bafd5205209d49146c6aba
Bart van Deenen <bartvandeenen@streammachine.io>
2023-11-21 13:32:44 +0100

    feat(feature/pace-57): adds some hierarchy to ODD

--- app/src/main/kotlin/com/getstrm/pace/catalogs/OpenDataDiscovery.kt
+++ app/src/main/kotlin/com/getstrm/pace/catalogs/OpenDataDiscovery.kt
@@ -16,82 +16,49 @@ class OpenDataDiscoveryCatalog(configuration: CatalogConfiguration) : DataCatalo
     private val searchClient = SearchApi(configuration.serverUrl)
     private val datasetsClient = DataSetApi(configuration.serverUrl)
     private val dataSourceClient = DataSourceApi(configuration.serverUrl)
-
     private val dataSources = getAllDataSources().associateBy { it.id }
 
-    override suspend fun listDatabases(): List<Database> {
-        val searchRef = searchClient.search(
-            SearchFormData(
-                query = "",
-                filters = SearchFormDataFilters(
-                    entityClasses = listOf(
-                        SearchFilterState(
-                            entityName = "DATA_SET",
-                            // selected means that this is included in the search results
-                            selected = true,
-                            // Not sure why the entity id is necessary, I think it
-                            // refers to the entity class id, which "should be" a static
-                            // value, in case of the entity type DATA_SET it is 1
-                            entityId = 1,
-                        ),
-                    ),
-                ),
-            ),
-        )
-
-        return getAllSearchResults(searchRef.searchId).map {
-            val dataSource = dataSources[it.dataSource.id]?.name ?: "unknown"
-            Database(this, it.id.toString(), dataSource, it.externalName)
+    /**
+     * We're doing a weird workaround, where we search all datasets, and then find the data sources
+     * that contain any datasets.
+     */
+    override suspend fun listDatabases(): List<Database> =
+        dataSources.mapNotNull { (_, dataSource) ->
+            val searchId = searchDataSetsInDataSource(dataSource).searchId
+            if (searchClient.getSearchResults(searchId, 1, 1).items.isNotEmpty()) dataSource else null
+        }.map {
+            Database(this, it, it.id.toString(), it.namespace?.name ?: "", it.name)
         }
-    }
 
-    private fun getAllSearchResults(searchId: UUID, page: Int = 1, size: Int = 100): List<DataEntity> {
-        val searchResults = searchClient.getSearchResults(searchId, page, size)
-
-        return if (searchResults.items.size == size) {
-            searchResults.items + getAllSearchResults(searchId, page + 1, size)
-        } else {
-            searchResults.items
-        }
-    }
-
-    private fun getAllDataSources(page: Int = 1, size: Int = 100): List<DataSource> {
-        val dataSources = dataSourceClient.getDataSourceList(page, size)
-
-        return if (dataSources.items.size == size) {
-            dataSources.items + getAllDataSources(page + 1, size)
-        } else {
-            dataSources.items
-        }
-    }
-
-    override fun close() {
-    }
-
-    class Database(override val catalog: OpenDataDiscoveryCatalog, id: String, dbType: String, displayName: String) :
-        DataCatalog.Database(catalog, id, dbType, displayName) {
-        /** Effectively a noop, but that's because ODD's data model isn't hierachical
-         */
+    class Database(
+        override val catalog: OpenDataDiscoveryCatalog,
+        val dataSource: DataSource,
+        id: String,
+        dbType: String,
+        displayName: String
+    ) : DataCatalog.Database(catalog, id, dbType, displayName) {
         override suspend fun getSchemas(): List<Schema> {
-            return listOf(Schema(catalog, this, id, dbType.orEmpty()))
+            return listOf(Schema(catalog, this, "schema", dataSource.name))
         }
     }
 
     class Schema(
         private val catalog: OpenDataDiscoveryCatalog,
-        database: DataCatalog.Database,
+        val oddDatabase: Database,
         id: String,
         name: String,
-    ) :
-        DataCatalog.Schema(database, id, name) {
+    ) : DataCatalog.Schema(oddDatabase, id, name) {
         override suspend fun getTables(): List<DataCatalog.Table> {
-            return listOf(Table(catalog, this, id, name))
+
+            val ref = catalog.searchDataSetsInDataSource(oddDatabase.dataSource)
+            val x = catalog.getAllSearchResults(ref.searchId)
+            return x.map{Table(catalog, this, "${it.id}", it.externalName)}
         }
     }
 
+
     class Table(private val catalog: OpenDataDiscoveryCatalog, schema: DataCatalog.Schema, id: String, name: String) :
         DataCatalog.Table(schema, id, name) {
-
         private fun getAllParents(parentId: Long, fieldsById: Map<Long, DataSetField>): List<Long> {
             val parent = checkNotNull(fieldsById[parentId]) { "The parent field should exist" }
 
@@ -101,9 +68,8 @@ class OpenDataDiscoveryCatalog(configuration: CatalogConfiguration) : DataCatalo
                 listOf(parent.id)
             }
         }
-
         override suspend fun getDataPolicy(): DataPolicy? {
-            val datasetStructure = catalog.datasetsClient.getDataSetStructureLatest(schema.database.id.toLong())
+            val datasetStructure = catalog.datasetsClient.getDataSetStructureLatest(id.toLong())
             val fields = datasetStructure.fieldList
             val fieldsById = fields.associateBy { it.id }
 
@@ -131,6 +97,62 @@ class OpenDataDiscoveryCatalog(configuration: CatalogConfiguration) : DataCatalo
             policyBuilder.metadataBuilder.description = schema.database.dbType
 
             return policyBuilder.build()
+        }
+    }
+
+    private fun getAllDataSources(page: Int = 1, size: Int = 100): List<DataSource> {
+        val dataSources = dataSourceClient.getDataSourceList(page, size)
+        return if (dataSources.items.size == size) {
+            dataSources.items + getAllDataSources(page + 1, size)
+        } else {
+            dataSources.items
+        }
+    }
+
+    override fun close() {
+    }
+
+
+    /**
+     * search all the datasets in one particular dataSource.
+     */
+    private fun searchDataSetsInDataSource(dataSource: DataSource): SearchFacetsData =
+        searchClient.search(
+        SearchFormData(
+            query = "",
+            filters = SearchFormDataFilters(
+                entityClasses = listOf(
+                    SearchFilterState(
+                        entityName = "DATA_SET",
+                        // selected means that this is included in the search results
+                        selected = true,
+                        // Not sure why the entity id is necessary, I think it
+                        // refers to the entity class id, which "should be" a static
+                        // value, in case of the entity type DATA_SET it is 1
+                        entityId = 1,
+                    ),
+                ),
+                datasources = listOf(
+                    SearchFilterState(
+                        entityName = dataSource.name,
+                        entityId = dataSource.id,
+                        selected = true,
+                    )
+                )
+            ),
+        ),
+    )
+
+    /**
+     * recursively get all results for a given search id.
+     */
+    private fun getAllSearchResults(searchId: UUID, page: Int = 1, size: Int = 100): List<DataEntity> {
+        val searchResults = searchClient.getSearchResults(searchId, page, size)
+
+        return if (searchResults.items.size == size) {
+            searchResults.items + getAllSearchResults(searchId, page + 1, size)
+        } else {
+            searchResults.items
         }
     }
 }
