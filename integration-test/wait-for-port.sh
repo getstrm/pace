2d88f47029649de762d05ecb059d50c58dd17816
Bart van Deenen <bartvandeenen@streammachine.io>
2023-11-23 13:39:23 +0100

    feat(pace-66): make all in integration-test

---
+++ integration-test/wait-for-port.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+HOST=localhost
+PORT=50051
+MAX_WAIT_TIME=15
+SECONDS_WAITED=0
+
+while [ $SECONDS_WAITED -lt $MAX_WAIT_TIME ] && ! nc -z "$HOST" "$PORT"; do
+    sleep 1
+    SECONDS_WAITED=$((SECONDS_WAITED + 1))
+
+    if [ $SECONDS_WAITED -gt $MAX_WAIT_TIME ]; then
+        echo "Maximum wait time exceeded. TCP socket is still not available."
+        exit 1
+    fi
+done
