81f10f06b03383e593e394169bb5257523aff847
Ivan Plantevin <ivan@getstrm.com>
2024-01-23 10:51:02 +0100

    feat(pace-133): simplify data policy versioning in upsert call (#163)
    Now, data policy versions will always increment by default. The latest version
    does not need to be provided anymore upon upsert.
    
    Add following to your application configuration to revert to the previous behaviour where upon upsert, the latest version must be set in the data policy.
    
    ```
    app:
      data-policies:
        auto-increment-version: false
    ```

--- app/src/main/kotlin/com/getstrm/pace/config/AppConfiguration.kt
+++ app/src/main/kotlin/com/getstrm/pace/config/AppConfiguration.kt
@@ -8,6 +8,7 @@ data class AppConfiguration(
     val exposeApplicationExceptions: Boolean = false,
     val defaultViewSuffix: String = "",
     val catalogs: List<CatalogConfiguration> = emptyList(),
+    val dataPolicies: DataPoliciesConfiguration = DataPoliciesConfiguration(),
 )
 
 data class CatalogConfiguration(
@@ -18,4 +19,8 @@ data class CatalogConfiguration(
     val userName: String?,
     val password: String?,
     val fetchSize: Int? = 1,
+)
+
+data class DataPoliciesConfiguration(
+    val autoIncrementVersion: Boolean = true,
 )
