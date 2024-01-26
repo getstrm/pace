df182404131a6348485aef5678dc6a3b8fac6485
Bart van Deenen <bartvandeenen@streammachine.io>
2024-01-26 14:40:51 +0100

    refactor(pace-138): removed unneeded constructor argument

--- app/src/main/kotlin/com/getstrm/pace/service/ProcessingPlatformsService.kt
+++ app/src/main/kotlin/com/getstrm/pace/service/ProcessingPlatformsService.kt
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Component
 @Component
 class ProcessingPlatformsService(
     config: ProcessingPlatformsConfiguration,
-    val globalTransformsService: GlobalTransformsService,
 ) {
     final val platforms: Map<String, ProcessingPlatformClient>
 
