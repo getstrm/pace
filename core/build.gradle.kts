6e56df2882262c553b55f59bea821ed58047acee
Ivan Plantevin <ivan@getstrm.com>
2024-02-09 16:41:56 +0100

    chore(pace-155): cleanup core module and gradle build file

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -24,11 +24,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("net.devh:grpc-server-spring-boot-starter:2.15.0.RELEASE")
 
-    // TODO remove once we upgrade Spring: override SnakeYAML dependency, as the one managed by
-    // Spring is too old and is vulnerable
-    implementation("org.yaml:snakeyaml:2.2")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.postgresql:postgresql")
     implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml")
@@ -36,39 +32,19 @@ dependencies {
     implementation("com.h2database:h2")
     // TODO Remove once this bug is fixed: https://github.com/Kotlin/kotlinx.coroutines/issues/3958
     runtimeOnly("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
 
     // Self-managed dependencies
-    implementation("com.databricks:databricks-sdk-java:0.17.1")
     implementation("com.github.drapostolos:type-parser:0.8.1")
-    implementation("com.microsoft.sqlserver:mssql-jdbc:12.4.2.jre11")
-
-    implementation("com.nimbusds:nimbus-jose-jwt:9.37.3")
-    implementation("org.bouncycastle:bcpkix-jdk18on:1.77")
-
-    implementation(enforcedPlatform("com.google.cloud:libraries-bom:26.31.0"))
-    implementation("com.google.cloud:google-cloud-bigquery")
-    implementation("com.google.cloud:google-cloud-datacatalog")
-    implementation("com.google.cloud:google-cloud-datalineage")
-
-    implementation("build.buf.gen:getstrm_pace_grpc_java:1.61.0.1.$generatedBufDependencyVersion")
-    implementation("build.buf.gen:getstrm_pace_grpc_kotlin:1.4.1.1.$generatedBufDependencyVersion")
+    implementation("com.google.protobuf:protobuf-java-util:3.25.2")
     implementation(
-        "build.buf.gen:getstrm_pace_protocolbuffers_java:25.2.0.1.$generatedBufDependencyVersion"
+        "build.buf.gen:getstrm_pace_protocolbuffers_kotlin:25.2.0.1.$generatedBufDependencyVersion",
     )
     implementation("build.buf:protovalidate:0.1.9")
-
-    implementation("com.apollographql.apollo3:apollo-runtime:3.8.2")
-
-    implementation("com.aallam.openai:openai-client:3.6.3")
-    implementation(platform("io.ktor:ktor-bom:2.3.8"))
-    runtimeOnly("io.ktor:ktor-client-okhttp")
-    implementation("io.ktor:ktor-client-logging")
 
     // Test dependencies
     testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
-    testImplementation("io.mockk:mockk:1.13.9")
-    testImplementation("io.zonky.test:embedded-postgres:2.0.6")
 }
 
 tasks.findByName("spotlessApply")?.dependsOn("spotlessKotlin")
--- core/src/main/docker/startup.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/bin/envoy --config-path /app/envoy.yaml &
-
-exec java -Dlog4j2.formatMsgNoLookups=true -jar app.jar
